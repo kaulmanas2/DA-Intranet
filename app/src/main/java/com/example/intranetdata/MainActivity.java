49d95f77f65c85dac2afc932c17742301cbba512
Manas <manaskaulcocid@gmail.com>
2020-04-23 16:13:52 +0530

    added comments, icons

--- app/src/main/java/com/example/intranetdata/MainActivity.java
+++ app/src/main/java/com/example/intranetdata/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PatternMatcher;
-import android.service.autofill.FieldClassification;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,17 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.Format;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,19 +27,24 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
-    ArrayList<String> links = new ArrayList<>();
-    ArrayList<String> title = new ArrayList<>();
-    ArrayList<Integer> icons = new ArrayList<>();
-    ArrayList<String> dateMod = new ArrayList<>();
-    ArrayList<String> timeMod = new ArrayList<>();
+    ArrayList<String> links = new ArrayList<>();    // links to the pages
+    ArrayList<String> title = new ArrayList<>();    // title of the pages
+    ArrayList<Integer> icons = new ArrayList<>();   // icons based upon the file extension
+    ArrayList<String> dateMod = new ArrayList<>();  // modify date
+    ArrayList<String> timeMod = new ArrayList<>();  // modify time
 
+    // base url is used when going to the parent directory
     String baseUrl = "http://intranet.daiict.ac.in/";
+    // current url is used to go into a directory or open a file
     String currentUrl = "http://intranet.daiict.ac.in/~daiict_nt01/";
 
+    // custom array list adapter
     MyListAdapter adapter;
 
+    // to check back button pressed twice
     boolean doubleBackPressed = false;
 
+    // map containing the extensions and corresponding icon value (R.drawble. )
     HashMap<String, Integer> map = new HashMap<>();
 
     @Override
@@ -54,20 +52,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // list view showing directories and files
         listView = (ListView) findViewById(R.id.listView);
+        // custom array adapter
         adapter = new MyListAdapter(this, title, icons, dateMod, timeMod);
         listView.setAdapter(adapter);
 
+        // show back icon on action
+        // used to go back to the parent directory
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        putIntoMap();
+        putIntoMap();   // put the icons values corresponding to the extension
 
         String res = "";
         try {
             DownloadData downloadData = new DownloadData();
             res = downloadData.execute(baseUrl+"~daiict_nt01/").get();
 
+            // get the required titles, links, date and time from the parsed HTML
             getDataFromHTML(res);
         }
         catch (Exception e) {
@@ -78,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // go to new url
+                // links array list contains link to parent directory at 0th position
+                // so i+1 to get the correct link
                 goToNewURL(i+1);
             }
         });
@@ -85,13 +91,14 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // toast the title
                 Toast.makeText(MainActivity.this, title.get(i), Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
     }
 
-    public class DownloadData extends AsyncTask <String, Void, String> {
+    public class DownloadData extends AsyncTask <String, Void, String> {    // parse the html file from URL
         @Override
         protected String doInBackground(String... strings) {
             URL url;
@@ -127,10 +134,12 @@ public class MainActivity extends AppCompatActivity {
         try {
             String newUrl;
 
+            // go to parent directory
             if (i == 0) {
                 newUrl = baseUrl + links.get(i);
                 currentUrl = baseUrl + links.get(i);
             }
+            // go to a directory or open a file
             else {
                 newUrl = currentUrl + links.get(i);
                 currentUrl = currentUrl + links.get(i);
@@ -138,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
 
             resNew = downloadData.execute(newUrl).get();
 
+            // get data from the parsed HTML
             getDataFromHTML(resNew);
         }
         catch (Exception e) {
@@ -147,33 +157,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getDataFromHTML (String res) {
+        // clear all array list so as to add new data
         title.clear();
         links.clear();
         icons.clear();
-
         dateMod.clear();
         timeMod.clear();
 
+        // get all the links to the directory and files
         Pattern p = Pattern.compile("<td><a href=\"(.*?)\">");
         Matcher m = p.matcher(res);
         while (m.find()) {
             links.add(m.group(1));
         }
 
+        // get the name of all directories and the files
         p = Pattern.compile("\">(.[^\">]*?)</a></td>");
         m = p.matcher(res);
         while (m.find()) {
             String data = m.group(1);
+            // if it is a directory
             if (data.charAt(data.length()-1) == '/') {
                 title.add(data.substring(0, data.length()-1));
                 // It is a folder
+                // add icon to array list
                 icons.add(map.get("folder"));
 
             }
+            // if it is a file
             else {
-                title.add(data);
+                // if it is a temporary file
+                if (data.charAt(0) != '~') {
+                    title.add(data);
+                }
                 // It is a file
-
+                // get the extension of the file and set corresponding icon matching the extension
                 String type = "";
                 if (data.charAt(data.length()-4) == '.') {
                     type = data.substring(data.length()-4);
@@ -181,36 +199,41 @@ public class MainActivity extends AppCompatActivity {
                 else if (data.charAt(data.length()-5) == '.') {
                     type = data.substring(data.length()-5);
                 }
-
+                // add icon to array list
                 icons.add(map.getOrDefault(type, R.drawable.fileicon));
             }
         }
 
         // "24-Apr-2019 12:40  "
-
+        // get the modified date and time
         p = Pattern.compile("</td><td align=\"right\">(.*?)</td><td align=\"right\">");
         m = p.matcher(res);
         while (m.find()) {
             String data = m.group(1);
-            String date = data.substring(0, 11);
-            String time = data.substring(12, 17);
+            String date = data.substring(0, 11);    // extract the date
+            String time = data.substring(12, 17);   // extract the time
             dateMod.add(date);
             timeMod.add(time);
         }
 
+        // remove the parent directory fro the list
         title.remove(0);
         icons.remove(0);
 
         Log.i("Titles --------->", title.toString());
         Log.i("Links --------->", links.toString());
 
+        // notify change in the list
         adapter.notifyDataSetChanged();
+        // scroll to the top of the list
         listView.smoothScrollToPosition(0);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // back button on action bar clicked
         if (item.getItemId() == android.R.id.home) {
+            // do nothing if we are already on the home page
             if (!links.get(0).equals("/")) {
                 goToNewURL(0);
             }
@@ -220,6 +243,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        // double tap back button to exit the app
         if (doubleBackPressed) {
             super.onBackPressed();
             return;
@@ -235,6 +259,7 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    // put the extensions nad the corresponding icon value
     public void putIntoMap () {
         map.put("folder", R.drawable.foldericon);
         map.put("file", R.drawable.fileicon);
@@ -245,6 +270,7 @@ public class MainActivity extends AppCompatActivity {
         map.put(".ppt", R.drawable.ppt);
         map.put(".pptx", R.drawable.pptx);
         map.put(".jpg", R.drawable.jpg);
+        map.put(".png", R.drawable.png);
         map.put(".log", R.drawable.log);
         map.put(".rar", R.drawable.rar);
         map.put(".sql", R.drawable.sql);
